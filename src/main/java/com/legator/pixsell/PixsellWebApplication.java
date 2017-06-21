a31f01b07bc814ef98530e74302dc9beb05f9535
tharindu99 <madushanka.best@gmail.com>
2017-06-21 13:18:51 +0530

    project initialized

---
+++ src/main/java/com/legator/pixsell/PixsellWebApplication.java
@@ -0,0 +1,12 @@
+package com.legator.pixsell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixsellWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixsellWebApplication.class, args);
+	}
+}
